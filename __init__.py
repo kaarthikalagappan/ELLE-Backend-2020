b44f026a44cb11a3c0188657ef82f22a568d9354
michaelasantiago <michael.alec.santiago@gmail.com>
2020-07-28 16:26:20 -0400

    Added two new score reporting endpoints, renamed old modulestats

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ from resources.logged_answer import LoggedAnswer
 from resources.sessions import Session, SearchSessions, End_Session
 from resources.question import Question, Answer, SearchType, SearchText, DeleteQuestion, DeleteAnswer, Modify
 from resources.modules import Modules, ModuleQuestions, Module, AttachQuestion, AttachTerm
-from resources.stats import ModuleStats
+from resources.stats import ModuleReport, ModuleStats, PlatformStats
 from db import mysql
 from db_utils import *
 from pathlib import Path
@@ -94,7 +94,9 @@ api.add_resource(Session, '/session')
 api.add_resource(SearchSessions, '/searchsessions')
 api.add_resource(End_Session, '/endsession')
 api.add_resource(GameLog, '/gamelog')
+api.add_resource(ModuleReport, '/modulereport')
 api.add_resource(ModuleStats, '/modulestats')
+api.add_resource(PlatformStats, '/platformstats')
 
 
 if __name__ == '__main__':
--- resources/stats.py
+++ resources/stats.py
@@ -6,9 +6,56 @@ from flask_jwt_extended import jwt_required, get_jwt_identity, get_raw_jwt
 from db import mysql
 from db_utils import *
 from utils import *
+from datetime import datetime, timedelta
 import os.path
 
-class ModuleStats(Resource):
+def get_platforms():
+    query = "SELECT DISTINCT platform FROM session"
+    return get_from_db(query)
+
+def get_module_headers():
+    query = "SELECT DISTINCT moduleID, name FROM module"
+    return get_from_db(query)
+
+def query_sessions(query):
+    result = get_from_db(query)
+    sessions = []
+    for row in result:
+        session = {}
+        session['sessionID'] = row[0]
+        session['userID'] = row[1]
+        session['moduleID'] = row[2]
+        session['sessionDate'] = row[3]
+        session['playerScore'] = row[4]
+        session['startTime'] = row[5]
+        session['endTime'] = row[6]
+        session['platform'] = row[7]
+        # Ignoring unfinished sessions
+        if session['endTime'] != None:
+            sessions.append(session)
+    return sessions
+
+def get_averages(sessions):
+    scoreTotal = 0
+    timeTotal = 0
+    for session in sessions:
+        # Accumulating score
+        scoreTotal += session['playerScore']
+        startDateTime = datetime.strptime(session['startTime'], '%H:%M')
+        # Accumulating time
+        endDateTime = datetime.strptime(session['endTime'], '%H:%M')
+        elapsedTime = endDateTime - startDateTime
+        timeTotal += timedelta(elapsedTime)
+    # Returning statistics object
+    stat = {}
+    stat['averageScore'] = scoreTotal / sessions.length
+    stat['averageSessionLength'] = timeTotal / sessions.length
+    return stat
+        
+
+
+# Returns a list of sessions associated with the given module
+class ModuleReport(Resource):
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('moduleID', required=True, type=str, help="Please supply the ID of the module you wish to look up.")
@@ -16,19 +63,7 @@ class ModuleStats(Resource):
         module_id = data['moduleID']
         # Getting all sessions associated with a module
         query = f"SELECT * FROM session WHERE moduleID = '{module_id}'"
-        result = get_from_db(query)
-        sessions = []
-        for row in result:
-            session = {}
-            session['sessionID'] = row[0]
-            session['userID'] = row[1]
-            session['moduleID'] = row[2]
-            session['sessionDate'] = row[3]
-            session['playerScore'] = row[4]
-            session['startTime'] = row[5]
-            session['endTime'] = row[6]
-            session['platform'] = row[7]
-            sessions.append(session)
+        sessions = query_sessions(query)
         # Getting all logged_answers associated with each session
         for session in sessions:
             session_id = session['sessionID']
@@ -45,4 +80,49 @@ class ModuleStats(Resource):
                 session['logged_answers'].append(logged_answer)
         return sessions
 
+# Provides a list of all platform names currently used by sessions in the database
+class PlatformNames(Resource):
+    @jwt_required
+    def get(self):
+        result = get_platforms()
+        return result
+        
 
+# Provides the average score and session duration for the given module
+class ModuleStats(Resource):
+    @jwt_required
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('moduleID', required=True, type=str, help="Please supply the ID of the module you wish to look up.")
+        data = parser.parse_args()
+        module_id = data['moduleID']
+        platforms = get_platforms()
+        stats = {}
+        for platform in platforms:
+            query = f"SELECT * FROM session WHERE moduleID = '{module_id} AND platform = {platform}'"
+            sessions = query_sessions(query)
+            stat = get_averages(sessions)
+            stat['platform'] = platform
+            stats.append(stat)
+        return stats
+        
+
+
+
+# Provides the average scores and session durations for the given game
+class PlatformStats(Resource):
+    @jwt_required
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('platform', required=True, type=str, help="Please supply the name of the platform you wish to look up.")
+        data = parser.parse_args()
+        platform = data['platform']
+        modules = get_module_headers()
+        stats = {}
+        for module in modules:
+            query = f"SELECT * FROM session WHERE moduleID = '{module[0]} AND platform = {platform}'"
+            sessions = query_sessions(query)
+            stat = get_averages(sessions)
+            stat['module'] = module[1]
+            stats.append(stat)
+        return stats
